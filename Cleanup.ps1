aee384d5376ee80e5a4029040cc09a7121bcf83f
Rich <114684848+EugeneBelford1995@users.noreply.github.com>
2024-03-20 14:51:17 -0400

    Update Cleanup.ps1

--- Cleanup.ps1
+++ Cleanup.ps1
@@ -1,15 +1,27 @@
-﻿#Cleanup/delete a VM
-#Remove-VMHardDiskDrive -VMName TestServer -ControllerType SCSI -ControllerNumber 0 -ControllerLocation 0
+#Cleanup/delete a VM
+#Remove-VMHardDiskDrive -VMName $VMName -ControllerType SCSI -ControllerNumber 0 -ControllerLocation 0
 
+Function Cleanup-VM
+{
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $VMName,
+         [Parameter(Mandatory=$false, Position=1)]
+         [string] $IP
+    )
+
+Write-Host "This will delete the VM, it's HD, and it's AD account. Only run this if you are sure that you're done with the VM."
 #Alt
-Stop-VM -Name TestServer -Force
-Get-VMHardDiskDrive -VMName TestServer | Remove-VMHardDiskDrive
-Remove-VM -Name TestServer -Confirm
+Stop-VM -Name $VMName -Force
+Get-VMHardDiskDrive -VMName $VMName | Remove-VMHardDiskDrive
+Remove-VM -Name $VMName -Confirm
 Remove-Item C:\VM_Stuff_Share\LocalCredential.xml
-Remove-Item C:\Hyper-V_VMs\TestServer -Recurse
-Remove-ADComputer "TestServer"
+Remove-Item "C:\Hyper-V_VMs\$VMName" -Recurse
+Remove-ADComputer "$VMName" -Confirm
 
 #https://pscustomobject.github.io/powershell/howto/PowerShell-ISE-Clear-Variables/
 #Remove-Variable * -ErrorAction SilentlyContinue
 #$error.Clear()
 #Remove-Module *
+}
